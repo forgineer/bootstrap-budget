e4e81b876ee8e8eb6aababe525daf25d885d0d3a
forgineer <blake.phillips86@gmail.com>
2022-06-25 10:12:41 -0500

    Moving Files Around into Flask Project Format

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi
+web: gunicorn flaskr.app:app
--- sql/ALTER_TABLES_FOREIGN_KEYS.sql
+++ sql/ALTER_TABLES_FOREIGN_KEYS.sql
@@ -10,15 +10,15 @@ ALTER TABLE public.account ADD CONSTRAINT account_fk_1 FOREIGN KEY (user_id) REF
 
 ALTER TABLE public.budget ADD CONSTRAINT budget_fk FOREIGN KEY (user_id) REFERENCES public.users(user_id);
 
-ALTER TABLE public.user_budget ADD CONSTRAINT user_budget_fk FOREIGN KEY (budget_id) REFERENCES public.budget(budget_id);
-ALTER TABLE public.user_budget ADD CONSTRAINT user_budget_fk_1 FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+ALTER TABLE public.budget_item ADD CONSTRAINT budget_item_fk FOREIGN KEY (budget_id) REFERENCES public.budget(budget_id);
+ALTER TABLE public.budget_item ADD CONSTRAINT budget_item_fk_1 FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+
+ALTER TABLE public.dashboard ADD CONSTRAINT dashboard_fk FOREIGN KEY (budget_id) REFERENCES public.budget(budget_id);
+ALTER TABLE public.dashboard ADD CONSTRAINT dashboard_fk_1 FOREIGN KEY (budget_item_id) REFERENCES public.budget_item(budget_item_id);
 
 ALTER TABLE public."transaction" ADD CONSTRAINT transaction_fk FOREIGN KEY (account_id) REFERENCES public.account(account_id);
 ALTER TABLE public."transaction" ADD CONSTRAINT transaction_fk_1 FOREIGN KEY (budget_item_id) REFERENCES public.budget_item(budget_item_id);
 ALTER TABLE public."transaction" ADD CONSTRAINT transaction_fk_2 FOREIGN KEY (user_id) REFERENCES public.users(user_id);
 
-ALTER TABLE public.dashboard ADD CONSTRAINT dashboard_fk FOREIGN KEY (budget_id) REFERENCES public.budget(budget_id);
-ALTER TABLE public.dashboard ADD CONSTRAINT dashboard_fk_1 FOREIGN KEY (budget_item_id) REFERENCES public.budget_item(budget_item_id);
-
-ALTER TABLE public.budget_item ADD CONSTRAINT budget_item_fk FOREIGN KEY (budget_id) REFERENCES public.budget(budget_id);
-ALTER TABLE public.budget_item ADD CONSTRAINT budget_item_fk_1 FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+ALTER TABLE public.user_budget ADD CONSTRAINT user_budget_fk FOREIGN KEY (budget_id) REFERENCES public.budget(budget_id);
+ALTER TABLE public.user_budget ADD CONSTRAINT user_budget_fk_1 FOREIGN KEY (user_id) REFERENCES public.users(user_id);
