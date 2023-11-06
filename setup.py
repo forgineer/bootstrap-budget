2cd92b44e7191cfe487683616623347595e37c88
Blake Phillips <23227382+forgineer@users.noreply.github.com>
2023-11-06 00:06:43 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md") as f:
 
 setup(
     name='bootstrap-budget',
-    version='0.0.1',
+    version='0.0.2',
     author='forgineer',
     description='A simple financial application to help you pull your budget up by its bootstraps.',
     long_description=long_description,
