6859f3abd52a3ffa5e7f11126e4b29c9946a71b9
Blake Phillips <23227382+forgineer@users.noreply.github.com>
2024-03-10 18:28:38 -0500

    Improved error flashing and added reset password modal

--- src/bootstrap_budget/user.py
+++ src/bootstrap_budget/user.py
@@ -28,18 +28,35 @@ def update() -> Response | str:
     """
     user: User = g.user
 
-    #TODO: Sanitize form contents
-    user.first_name = request.form['first_name']
-    user.middle_name = request.form['middle_name']
-    user.last_name = request.form['last_name']
-    user.address_line_1 = request.form['address_line_1']
-    user.address_line_2 = request.form['address_line_2']
-    user.city = request.form['city']
-    user.state = request.form['state']
-    user.zipcode = request.form['zipcode']
-    user.email = request.form['email']
-    user.phone_number = request.form['phone_number']
+    try:
+        user.first_name = request.form['first_name']
+        user.middle_name = request.form['middle_name']
+        user.last_name = request.form['last_name']
+        user.address_line_1 = request.form['address_line_1']
+        user.address_line_2 = request.form['address_line_2']
+        user.city = request.form['city']
+        user.state = request.form['state']
+        user.zipcode = request.form['zipcode']
+        user.email = request.form['email']
+        user.phone_number = request.form['phone_number']
+
+        flash('User profile was successfully saved.', 'info')
+    except Exception as e:
+        flash(f'User profile failed to save: {e}', 'error')
 
     g.user = user
+
+    return redirect(request.referrer)
+
+
+@bp.route("/reset-password", methods=["POST"])
+@login_required
+def reset_password() -> Response | str:
+    """
+    Reset the password for a given user (by username) from 'Reset Password' modal.
+
+    :return: Back to current view after update.
+    """
+    flash('This does not do anything yet, Sorry :-/', 'warning')
 
     return redirect(request.referrer)
